c312d5c723286464589e7ea1e6a353379b3caf0a
Bill Barron <bill.r.barron@gmail.com>
2018-09-09 13:04:56 -0500

    Updated readme and initial commit of InstallNginx.ps1

---
+++ InstallNginx.ps1
@@ -0,0 +1,38 @@
+﻿Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$version = '1.15.3'
+$url = 'http://nginx.org/download/nginx-' + $version + '.zip'
+$zipfile = ($PSScriptRoot + '\nginx-' + $version + '.zip')
+$extractTo = ($PSScriptRoot + '\temp')
+$extracted = ($PSScriptRoot + '\temp\nginx-' + $version)
+$final = ($PSScriptRoot + '\temp\nginx')
+$start_time = Get-Date
+
+
+#Download Nginx
+Write-Output ("Downloading Nginx " + $version + ' to ' + $zipfile)
+Invoke-WebRequest -Uri $url -OutFile $zipfile
+Write-Output "Download Complete. Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+# Extract the zip files to a temporary folder
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $extractTo);
+Write-Output ('Extracted to ' + $extracted)
+
+
+# Remove the version number from the nginx folder
+Rename-Item -Path $extracted -NewName 'nginx'
+
+
+# Move it to the C:\ Drive
+try {
+    Write-Output "Moving files to c:\nginx"
+    Move-Item -Path $final -Destination 'c:\nginx'
+} catch {
+    Write-Output "You must run this script as administrator to copy nginx files folder to c:\nginx"
+}
+
+# Clean up temp files
+Write-Output "Cleaning up"
+Remove-Item -Path $extractTo
+Remove-Item -Path $zipfile
+Write-Output "Done. Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # powershell-nginx
 Powershell scripts for nginx
+
+InstallNginx.ps1 will download nginx 1.15.3, extract it and move it to c:\nginx.
+
+The version of nginx that gets installed canbe changed by editing the powershell variable `$version`
+```
+$version = '1.15.3'
+```
+
+This code is provided as is with no warranty whatsoever. Use at your own risk.
